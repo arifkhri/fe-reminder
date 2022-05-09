3fe52d9dc4a1365ec11fef56c9d71af60ce9aad3
aghni <aghniihsn@gmail.com>
2022-05-09 16:27:06 +0700

    lazy-load

---
+++ src/pages/Karyawan.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Karyawan() {
+  return (
+    <div>
+      <h2>Karyawan</h2>
+    </div>
+  );
+}
+
+export default Karyawan;
---
+++ src/pages/Pengguna.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Pengguna() {
+  return (
+    <div>
+      <h2>Pengguna</h2>
+    </div>
+  );
+}
+
+export default Pengguna;
