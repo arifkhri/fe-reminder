654d10ebaf646eb0da1f73cb1cbfbfb94fc8b502
github_Tina <nurhamdirostina@gmail.com>
2022-05-19 17:57:35 +0700

    WIP list employee

---
+++ src/routes/Employee/components/ImportKaryawan/index.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import { Form, Input, Button, message, Spin } from "antd";
+
+import validation from "../../../../core/helpers/validation";
+// import axios from "../../../../core/helpers/axios";
+
+// import "./style.css";
+
+const ForgotPassword = (props) => {
+  const [loading, setLoading] = useState(false);
+  const [form] = Form.useForm();
+
+  const onFinish = (values) => {
+    setLoading(true)
+  };
+
+  return (
+    <div className="import-karyawan-component">
+      <p>
+        Untuk menggunakan fitur import karyawan anda dapat <br />
+        menggunakan <a href="URL">template ini.</a>
+      </p>
+      <Spin spinning={loading}>
+        <Form form={form} layout="vertical" onFinish={onFinish}>
+          <Form.Item
+            className="hide-required-sign"
+            label="Lampiran"
+            name="lampiran"
+            rules={[validation.required("please input your lampiran")]}>
+            <Input  />
+          </Form.Item>
+
+          <Form.Item wrapperCol={{ offset: 6 }}>
+            <Button type="link" htmlType="button" onClick>
+              Kembali
+            </Button>
+
+            <Button type="primary" htmlType="submit">
+              Import
+            </Button>
+          </Form.Item>
+        </Form>
+      </Spin>
+    </div>
+  );
+};
+
+export default ForgotPassword;
