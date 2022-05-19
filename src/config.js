d226d8378e3208e773df5c2f52ca955aa00b06ab
arifakhri <arifakhri97@gmail.com>
2022-05-19 07:55:32 +0700

    set enpoint to staging

--- src/config.js
+++ src/config.js
@@ -1,8 +1,7 @@
-// import dotenv from 'dotenv';
-// dotenv.config();
 
 const config = {
-  API_BE_URL: "http://localhost:4005",
+  API_BE_URL: "http://167.99.73.124:4005", // enable this for access to staging
+  // API_BE_URL: "http://localhost:4005",
   port: "3005"
 }
 
