8563ff81ee3c2348df780ec65e4483ca06ac87de
arifakhri <arifakhri97@gmail.com>
2022-05-27 08:21:15 +0700

    enhance notification

--- src/components/Notifications/index.jsx
+++ src/components/Notifications/index.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
-import { Badge } from "antd";
-import {ScheduleTwoTone, FileTextTwoTone} from "@ant-design/icons";
+import { ScheduleTwoTone, FileTextTwoTone } from "@ant-design/icons";
 import "./style.css";
 
 const Notifications = () => {
@@ -11,21 +10,21 @@ const Notifications = () => {
       title: "17 pengingat hari ini",
       desc: "Jangan lupa untuk menyelesaikan agenda anda",
       time: "5 Juli 2021",
-      icon: <FileTextTwoTone />,
+      icon: <img src="/images/reminder-notif.svg" />,
     },
     {
       id: 2,
       title: "Import agenda selesai",
       desc: "Report hasil import agenda dikirim ke email pengguna",
       time: "4 Juli 2021",
-      icon: <FileTextTwoTone />,
+      icon: <img src="/images/import-agenda-notif.svg" />,
     },
     {
       id: 3,
       title: "Import karyawan selesai",
       desc: "Report hasil karyawan dikirim ke email pengguna",
       time: "4 Juli 2021",
-      icon: <ScheduleTwoTone />,
+      icon: <img src="/images/import-employee-notif.svg" />,
     },
   ];
 
@@ -33,47 +32,34 @@ const Notifications = () => {
     <div className="notification container-scroll">
       <div>
         {/* <Spinner spinning={loading}> */}
-          <div className="notif-header p-4">
-            <h2>Notifikasi</h2>
-            {/* <div>
-              <span className="mr-4">Tandai Semua</span>
-              <span>
-                <Icon className="icon" name="ArrowsClockwise" />
-              </span>
-            </div> */}
-          </div>
-          <div className="notif-body content pl-4 pt-1">
-            {notifications.map((notif, i) => (
-              <div className="notif-item" key={notif.id}>
-                <div className="d-flex">
-                  <div className="mr-2">
-                    {/* <Badge dot  > */}
-                      {notif.icon}
-                      {/* <Icon
-                        className="icon"
-                        name="EnvelopeSimple"
-                        size="45px"
-                      /> */}
-                    {/* </Badge> */}
+        <div className="notif-header p-4">
+          <h2>Notifikasi</h2>
+        </div>
+        <div className="notif-body content px-4 pt-1">
+          {notifications.map((notif, i) => (
+            <div className="notif-item" key={notif.id}>
+              <div className="d-flex  justify-content-between">
+                <div className="mr-4">
+                  {notif.icon}
+                </div>
+                <div className="notif-msg">
+                  <div className="d-flex justify-content-between">
+                    <h3>{notif.title}</h3>
+                    <span>{notif.time}</span>
                   </div>
-                  <div className="notif-msg">
-                    <div className="d-flex">
-                      <h3>{notif.title}</h3>
-                      <span>{notif.time}</span>
-                    </div>
-                    <p>{notif.desc}</p>
-                  </div>
+                  <p>{notif.desc}</p>
                 </div>
               </div>
-            ))}
-          </div>
+            </div>
+          ))}
+        </div>
         {/* </Spinner> */}
       </div>
-      {/* <div className="notif-footer footer">
-        <div className="see-all">
+      <div className="notif-footer footer">
+        {/* <div className="see-all">
           <a>see all</a>
-        </div>
-      </div> */}
+        </div> */}
+      </div>
     </div>
   );
 };
